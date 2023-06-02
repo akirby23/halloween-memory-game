028c6ac549e12ec363bf7c22516cb9c86191344f
akirby23 <>
2023-06-02 20:09:00 +0000

    Fix CSS to ensure image appears when card is flipped & mirror image

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,31 @@ let section = document.querySelector("section");
 /**
  * Get the images
  */
-let getImages = () => [
-    { imgSrc: "assets/images/cards/halloween-balloons.png", name: "balloons" },
-    { imgSrc: "assets/images/cards/halloween-castle.png", name: "castle" },
-    { imgSrc: "assets/images/cards/halloween-cat.png", name: "cat" },
-    { imgSrc: "assets/images/cards/halloween-cauldron.png", name: "cauldron" },
-    { imgSrc: "assets/images/cards/halloween-crystal-ball.png", name: "crystal-ball" },
-    { imgSrc: "assets/images/cards/halloween-moon.png", name: "moon" },
-    { imgSrc: "assets/images/cards/halloween-pumpkin.png", name: "pumpkin" },
-    { imgSrc: "assets/images/cards/halloween-skeleton.png", name: "skeleton" },
-    { imgSrc: "assets/images/cards/halloween-spider.png", name: "spider" },
-    { imgSrc: "assets/images/cards/halloween-witch.png", name: "witch" },
-];
+function getImages() {
+    return [
+        { imgSrc: "assets/images/cards/halloween-balloons.png", name: "balloons" },
+        { imgSrc: "assets/images/cards/halloween-castle.png", name: "castle" },
+        { imgSrc: "assets/images/cards/halloween-cat.png", name: "cat" },
+        { imgSrc: "assets/images/cards/halloween-cauldron.png", name: "cauldron" },
+        { imgSrc: "assets/images/cards/halloween-crystal-ball.png", name: "crystal-ball" },
+        { imgSrc: "assets/images/cards/halloween-moon.png", name: "moon" },
+        { imgSrc: "assets/images/cards/halloween-pumpkin.png", name: "pumpkin" },
+        { imgSrc: "assets/images/cards/halloween-skeleton.png", name: "skeleton" },
+        { imgSrc: "assets/images/cards/halloween-spider.png", name: "spider" },
+        { imgSrc: "assets/images/cards/halloween-witch.png", name: "witch" },
+    ];
+}
 let images = getImages();
 let imagesPicklist = [...images, ...images];
 
 /**
  * Randomize the cards
  */
-let randomize = () => {
+function randomize() {
     let cardData = imagesPicklist;
     cardData.sort(() => Math.random() - 0.5);
     return cardData;
-};
+}
 
 
 /*
@@ -48,7 +50,7 @@ function cardGenerator() {
         card.appendChild(face);
         card.appendChild(back);
 
-        card.addEventListener("click", (e) => {
+        card.addEventListener("click", function (e) {
             card.classList.toggle("flipCard");
         });
     });
